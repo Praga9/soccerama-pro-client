5b491f47dfd07addfd776b8f5dd6b87a3dcbfb2f
ksa <samyn.kevin@gmail.com>
2016-08-08 08:45:59 +0200

    Augmentation limite d'appel par heure (720 à 1000)

--- src/main/java/pro/soccerama/client/proxy/SocceramaProxy.java
+++ src/main/java/pro/soccerama/client/proxy/SocceramaProxy.java
@@ -9,8 +9,8 @@ public abstract class SocceramaProxy {
 	public static final String COMMON_URL_PARAMS = "?api_token={api_token}&include={includes}";
 	public static final String VERSION = "v1.1";
 
-	private static final long HOUR = 3600;
-	private static final long CALL_BY_HOUR = 1000;
+	private static final double HOUR_IN_SECOND = 3600.0;
+	private static final double CALL_BY_HOUR = 1000.0;
 	private static long lastCall = 0;
 
 	/**
@@ -19,7 +19,7 @@ public abstract class SocceramaProxy {
 	protected void waitBeforeNextCall() {
 		synchronized (this) {
 			try {
-				while (System.currentTimeMillis() - lastCall < HOUR / CALL_BY_HOUR) {
+				while (System.currentTimeMillis() - lastCall < (HOUR_IN_SECOND / CALL_BY_HOUR)) {
 					TimeUnit.SECONDS.sleep(1);
 					// System.out.println(".");
 				}
