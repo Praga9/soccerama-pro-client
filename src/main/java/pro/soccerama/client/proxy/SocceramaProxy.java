40847ae7c83daef161f5209e1a8615b7cd5dc72d
ksa <samyn.kevin@gmail.com>
2016-08-08 16:37:20 +0200

    1000 appels/minutes

--- src/main/java/pro/soccerama/client/proxy/SocceramaProxy.java
+++ src/main/java/pro/soccerama/client/proxy/SocceramaProxy.java
@@ -9,8 +9,10 @@ public abstract class SocceramaProxy {
 	public static final String COMMON_URL_PARAMS = "?api_token={api_token}&include={includes}";
 	public static final String VERSION = "v1.1";
 
+	private static final double SECOND_IN_MILLISECOND = 1000.0;
 	private static final double HOUR_IN_SECOND = 3600.0;
 	private static final double CALL_BY_HOUR = 1000.0;
+	private static final double TIME_BETWEEN_TWO_CALLS = Math.ceil(HOUR_IN_SECOND / CALL_BY_HOUR * SECOND_IN_MILLISECOND * 1.05); // Marge de 5%
 	private static long lastCall = 0;
 
 	/**
@@ -19,9 +21,8 @@ public abstract class SocceramaProxy {
 	protected void waitBeforeNextCall() {
 		synchronized (this) {
 			try {
-				while (System.currentTimeMillis() - lastCall <= (Math.ceil(HOUR_IN_SECOND / CALL_BY_HOUR) * 1000)) {
-					TimeUnit.SECONDS.sleep(1);
-					// System.out.println(".");
+				while (System.currentTimeMillis() - lastCall <= TIME_BETWEEN_TWO_CALLS) {
+					TimeUnit.MILLISECONDS.sleep(1500);
 				}
 			} catch (final InterruptedException e) {
 				e.printStackTrace();
