f975398b9bf08af4d312313f60fcf682b6307cba
ksa <samyn.kevin@gmail.com>
2016-08-09 07:15:19 +0200

    Ajustement du nombre d'appel par heure

--- src/main/java/pro/soccerama/client/proxy/SocceramaProxy.java
+++ src/main/java/pro/soccerama/client/proxy/SocceramaProxy.java
@@ -12,7 +12,8 @@ public abstract class SocceramaProxy {
 	private static final double SECOND_IN_MILLISECOND = 1000.0;
 	private static final double HOUR_IN_SECOND = 3600.0;
 	private static final double CALL_BY_HOUR = 1000.0;
-	private static final double TIME_BETWEEN_TWO_CALLS = Math.ceil(HOUR_IN_SECOND / CALL_BY_HOUR * SECOND_IN_MILLISECOND * 1.05); // Marge de 5%
+	private static final double TIME_BETWEEN_TWO_CALLS = Math.ceil(HOUR_IN_SECOND / CALL_BY_HOUR * SECOND_IN_MILLISECOND);
+
 	private static long lastCall = 0;
 
 	/**
@@ -22,12 +23,14 @@ public abstract class SocceramaProxy {
 		synchronized (this) {
 			try {
 				while (System.currentTimeMillis() - lastCall <= TIME_BETWEEN_TWO_CALLS) {
-					TimeUnit.MILLISECONDS.sleep(1500);
+					TimeUnit.MILLISECONDS.sleep(100);
+
 				}
 			} catch (final InterruptedException e) {
 				e.printStackTrace();
 			}
 
+			System.out.println(System.currentTimeMillis());
 			lastCall = System.currentTimeMillis();
 			notifyAll();
 			return;
